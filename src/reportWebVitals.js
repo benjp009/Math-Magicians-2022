5b07b78c4b433d0a9793cf01cbea8a6d2070bf29
benjp009 <patin.benjamin@gmail.com>
2022-01-03 10:19:40 +0100

    Fix linters error 5

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,18 +1,18 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({
       getCLS,
       getFID,
       getFCP,
       getLCP,
-      getTTFB
+      getTTFB,
     }) => {
-        getCLS(onPerfEntry);
-        getFID(onPerfEntry);
-        getFCP(onPerfEntry);
-        getLCP(onPerfEntry);
-        getTTFB(onPerfEntry);
-      });
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
   }
 };
 
