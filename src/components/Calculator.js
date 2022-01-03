39162ad726d541262f343bf8666266af7dc363e5
benjp009 <patin.benjamin@gmail.com>
2022-01-03 15:09:46 +0100

    Fix Design to match template

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,44 +4,44 @@ import React from 'react';
 // eslint-disable-next-lin react/prefer-stateless-function
 function Calculator() {
   return (
-    <div className="Calculator">
-      <table>
+    <div className="calculator">
+      <table className="table">
         <thead>
           <tr>
-            <td colSpan="4">0</td>
+            <td colSpan="4" className="result">0</td>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>AC</td>
-            <td>±</td>
-            <td>%</td>
-            <td>÷</td>
+            <td className="number">AC</td>
+            <td className="number">±</td>
+            <td className="number">%</td>
+            <td className="calcSign">÷</td>
           </tr>
           <tr>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-            <td>×</td>
+            <td className="number">7</td>
+            <td className="number">8</td>
+            <td className="number">9</td>
+            <td className="calcSign">×</td>
           </tr>
           <tr>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-            <td>-</td>
+            <td className="number">4</td>
+            <td className="number">5</td>
+            <td className="number">6</td>
+            <td className="calcSign">-</td>
           </tr>
           <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-            <td>+</td>
+            <td className="number">1</td>
+            <td className="number">2</td>
+            <td className="number">3</td>
+            <td className="calcSign">+</td>
           </tr>
         </tbody>
         <tfoot>
           <tr>
-            <td colSpan="2">0</td>
-            <td>.</td>
-            <td>=</td>
+            <td colSpan="2" className="number">0</td>
+            <td className="number">.</td>
+            <td className="calcSign">=</td>
           </tr>
         </tfoot>
       </table>
