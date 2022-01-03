3b65b94cc8dc0d1b785f2d5b45431cf6dd8a386f
benjp009 <patin.benjamin@gmail.com>
2022-01-03 15:17:59 +0100

    Change app to class-based component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import Calculator from './components/Calculator';
 import React from 'react';
 
 class App  extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <Calculator />
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,52 +1,54 @@
 // import './Calculator.css';
 import React from 'react';
 
-// eslint-disable-next-lin react/prefer-stateless-function
-function Calculator() {
-  return (
-    <div className="calculator">
-      <table className="table">
-        <thead>
-          <tr>
-            <td colSpan="4" className="result">0</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="number">AC</td>
-            <td className="number">±</td>
-            <td className="number">%</td>
-            <td className="calcSign">÷</td>
-          </tr>
-          <tr>
-            <td className="number">7</td>
-            <td className="number">8</td>
-            <td className="number">9</td>
-            <td className="calcSign">×</td>
-          </tr>
-          <tr>
-            <td className="number">4</td>
-            <td className="number">5</td>
-            <td className="number">6</td>
-            <td className="calcSign">-</td>
-          </tr>
-          <tr>
-            <td className="number">1</td>
-            <td className="number">2</td>
-            <td className="number">3</td>
-            <td className="calcSign">+</td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan="2" className="number">0</td>
-            <td className="number">.</td>
-            <td className="calcSign">=</td>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
-  );
+// eslint-disable-next-lin prefer-stateless-function
+class Calculator  extends React.Component {
+  render() {
+    return (
+      <div className="calculator">
+        <table className="table">
+          <thead>
+            <tr>
+              <td colSpan="4" className="result">0</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="number">AC</td>
+              <td className="number">±</td>
+              <td className="number">%</td>
+              <td className="calcSign">÷</td>
+            </tr>
+            <tr>
+              <td className="number">7</td>
+              <td className="number">8</td>
+              <td className="number">9</td>
+              <td className="calcSign">×</td>
+            </tr>
+            <tr>
+              <td className="number">4</td>
+              <td className="number">5</td>
+              <td className="number">6</td>
+              <td className="calcSign">-</td>
+            </tr>
+            <tr>
+              <td className="number">1</td>
+              <td className="number">2</td>
+              <td className="number">3</td>
+              <td className="calcSign">+</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan="2" className="number">0</td>
+              <td className="number">.</td>
+              <td className="calcSign">=</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default Calculator;
