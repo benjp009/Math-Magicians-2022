c1f8af79dc95649a81fc8b3325146e7ada8db902
benjp009 <patin.benjamin@gmail.com>
2022-01-06 08:17:09 +0100

    Add react to calculator.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import React from 'react';
 import Calculator from './components/Calculator';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
       <Calculator />
     );
   }
 }
-
-export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,88 +2,97 @@
 import React from 'react';
 // eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
-// eslint-disable-next-line no-unused-vars
-import operate from '../logic/operate';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends React.Component {
+export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState((state) => calculate(state, e.target.textContent));
+  }
+
   render() {
+    const { next, total } = this.state;
     return (
       <div className="calculator">
         <table className="table">
           <thead>
             <tr>
-              <td colSpan="4" className="result">0</td>
+              <td colSpan="4" className="result">{next || total || 0}</td>
             </tr>
           </thead>
           <tbody>
             <tr>
               <td className="number">
-                <button type="button">Ac</button>
+                <button type="button" onClick={this.handleClick}>AC</button>
               </td>
               <td className="number">
-                <button type="button">±</button>
+                <button type="button" operation="±" onClick={this.handleClick}>±</button>
               </td>
               <td className="number">
-                <button type="button">%</button>
+                <button type="button" operation="%" onClick={this.handleClick}>%</button>
               </td>
               <td className="calcSign">
-                <button type="button">÷</button>
+                <button type="button" operation="÷" onClick={this.handleClick}>÷</button>
               </td>
             </tr>
             <tr>
               <td className="number">
-                <button type="button">7</button>
+                <button type="button" name="7" onClick={this.handleClick}>7</button>
               </td>
               <td className="number">
-                <button type="button">8</button>
+                <button type="button" name="8" onClick={this.handleClick}>8</button>
               </td>
               <td className="number">
-                <button type="button">9</button>
+                <button type="button" name="9" onClick={this.handleClick}>9</button>
               </td>
               <td className="calcSign">
-                <button type="button">×</button>
+                <button type="button" operation="×" onClick={this.handleClick}>×</button>
               </td>
             </tr>
             <tr>
               <td className="number">
-                <button type="button">4</button>
+                <button type="button" onClick={this.handleClick}>4</button>
               </td>
               <td className="number">
-                <button type="button">5</button>
+                <button type="button" onClick={this.handleClick}>5</button>
               </td>
               <td className="number">
-                <button type="button">6</button>
+                <button type="button" onClick={this.handleClick}>6</button>
               </td>
               <td className="calcSign">
-                <button type="button">-</button>
+                <button type="button" operation="-" onClick={this.handleClick}>-</button>
               </td>
             </tr>
             <tr>
               <td className="number">
-                <button type="button">1</button>
+                <button type="button" onClick={this.handleClick}>1</button>
               </td>
               <td className="number">
-                <button type="button">2</button>
+                <button type="button" onClick={this.handleClick}>2</button>
               </td>
               <td className="number">
-                <button type="button">3</button>
+                <button type="button" onClick={this.handleClick}>3</button>
               </td>
               <td className="calcSign">
-                <button type="button">+</button>
+                <button type="button" operation="+" onClick={this.handleClick}>+</button>
               </td>
             </tr>
           </tbody>
           <tfoot>
             <tr>
               <td colSpan="2" className="number">
-                <button type="button">0</button>
+                <button type="button" onClick={this.handleClick}>0</button>
               </td>
               <td className="number">
-                <button type="button">.</button>
+                <button type="button" onClick={this.handleClick}>.</button>
               </td>
               <td className="calcSign">
-                <button type="button">=</button>
+                <button type="button" onClick={this.handleClick}>=</button>
               </td>
             </tr>
           </tfoot>
@@ -92,5 +101,3 @@ class Calculator extends React.Component {
     );
   }
 }
-
-export default Calculator;
